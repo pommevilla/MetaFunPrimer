2903b3e0c8c798b8e603efa83dacb48f9ec7abda
pommevilla <pvillanueva13@gmail.com>
2019-11-11 13:05:34 -0500

    Add basic installation checker

---
+++ src/check_reqs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Checks to see if the packages required to run MetaFunPrimer packages are
+# available for use
+
+join () {
+    local IFS="$1"
+    shift   
+    echo "$*." | sed "s/,/, /g" 
+}
+
+echo "MetaFunPrimer"
+echo "============="
+
+
+printf "Checking if required programs are installed and executable.\n\n"
+
+requirements="diamond python blastx cd-hit clustalo-1.2.4-Ubuntu-x86_64"
+
+missing_reqs=()
+for req in $requirements
+do 
+    printf "Checking %s: " "$req"
+    if hash $req 2>/dev/null
+    then
+        echo "OK."
+    else
+        echo "Failed."
+        missing_reqs+=("$req")
+    fi
+done
+
+echo ""
+
+if [ "${#missing_reqs[@]}" -eq 0 ]
+then
+    echo "All required packages found. MetaFunPrimer is ready for use."
+    echo "See https://metafunprimer.readthedocs.io/en/latest/Tutorial.html for an introduction to the package."
+else
+    printf "Missing requirements: "
+    join , ${missing_reqs[@]}
+fi
+
