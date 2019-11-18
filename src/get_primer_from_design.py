a83f8c2a996530ea9f1ab9ff4a8e0d3641f18096
pommevilla <pvillanueva13@gmail.com>
2019-11-18 18:11:08 -0500

    Convert to Python 3, clean up code

--- src/get_primer_from_design.py
+++ src/get_primer_from_design.py
@@ -1,46 +1,44 @@
 #!/usr/bin/python
 
-# this script print primers in fasta format from output file of PrimerDesign
-# usage: python get_fasta_from_design.py output.design > output.fasta
-# make truseq form of primer???? -> need to check
+# Converts the primer sequences from the output of EcoFunPrimer into fasta format
+# Usage: python get_fasta_from_design.py input.rdp 
+# Input:
+#   - input.rdp: a file output from EcoFunPrimer
+# Output:
+#   - Prints out the sequences of input.rdp in fasta format to stdin
  
 import sys
-#import reverse_complement
-tru = 1
 
 def main():
     num = 0
     file = sys.argv[1]
-    #set = 0
     for line in open(file,'r'):
-        genename = file.split('.')[0]
         if line[:13] == "Primer Pair: " :
-            #set += 1
             prim = {}
             num += 1
             spl = line.strip().split("revOligo")
-            for i in range(0,len(spl)):
+            for i in range(0, len(spl)):
                 oli =  spl[i].split("seq=")
                 snum = 0
-                for j in range(1,len(oli)):
+                for j in range(1, len(oli)):
                     snum += 1
                     oneoli = oli[j].split("}")
                     cnu = str(num)
                     pnu = str(snum)
                     if len(cnu) == 1:
-                        cnu = "00"+cnu
+                        cnu = "00" + cnu
                     elif len(cnu) == 2:
                         cnu = "0" + cnu
                     
                     if len(pnu) == 1:
-                        pnu = "0"+pnu
+                        pnu = "0" + pnu
 
-                    if( i == 0):
-                        print ">F:"+file+".C"+cnu+"."+pnu+"F"
-                        print oneoli[0]
+                    if(i == 0):
+                        print( ">F:{}.C{}.{}F".format(file, cnu, pnu))
+                        print(oneoli[0])
                     else:
-                        print ">R:"+file+".C"+cnu+"."+pnu+"R"
-                        print oneoli[0]
+                        print(">R:{}.C{}.{}R".format(file, cnu, pnu))
+                        print(oneoli[0])
 
 
 if __name__ == '__main__':
